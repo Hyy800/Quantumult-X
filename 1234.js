f837c73aa20dd66c13175ef611bf1f1f1cfbf94d
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-10 12:18:49 +0800

    Update 1234.js

--- 1234.js
+++ 1234.js
@@ -10,7 +10,7 @@
 **************************************
 
 [rewrite_local]
-^http:\/\/101\.34\.60\.156:6543\/sk-api\/user\/open_vip url script-response-body https://raw.githubusercontent.com/Hyy800/Quantumult-X/refs/heads/Nana/1234.js
+^http:\/\/101\.34\.60\.156:6543\/sk-api\/user\/get_user_info\?user_id=471&user_name=????????\.com&type=my url script-response-body https://raw.githubusercontent.com/Hyy800/Quantumult-X/refs/heads/Nana/1234.js
 
 [mitm]
 hostname = 101.34.60.156
@@ -21,10 +21,26 @@ var hyy = JSON.parse($response.body);
 const Hyy666 = typeof $task !== "undefined";
 
 hyy = {
-  "msg" : "没钱也要充",
-  "code" : 1
-}
-;
+  "data" : {
+    "tgm" : "bjjlgb",
+    "user_points" : 0,
+    "user_pid_2" : 0,
+    "user_id" : 471,
+    "user_phone" : "",
+    "user_extend" : 0,
+    "user_reg_time" : 1728529657,
+    "user_name" : "2205333599@qq.com",
+    "group_id" : 3,
+    "user_email" : "2205333599@qq.com",
+    "user_pid" : 0,
+    "user_nick_name" : "请你洗干净菊花",
+    "user_qq" : "",
+    "user_random" : "fb211d90c8ec9a104c45275bdbb7681f",
+    "user_portrait" : "",
+    "user_end_time" : 1728616530
+  },
+  "code" : 200
+};
 $done({status: Hyy666 ? "HTTP/1.1 200 OK" : 200, body: JSON.stringify(hyy)});
 
 
