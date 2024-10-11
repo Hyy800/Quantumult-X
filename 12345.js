7ca87391c487222ffa77831d1aef6c6ad19e7215
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-11 22:20:11 +0800

    Update 12345.js

--- 12345.js
+++ 12345.js
@@ -1,6 +1,6 @@
 /*************************************
 
-项目名称：4k影视
+项目名称：淼播4k
 下载地址：
 更新日期：2024-10-10
 脚本作者：
@@ -10,26 +10,7 @@
 **************************************
 
 [rewrite_local]
-http:\/\/101\.34\.60\.156:6543\/sk-api\/user\/ulog_count\?user_id url script-response-body https://raw.githubusercontent.com/Hyy800/Quantumult-X/refs/heads/Nana/12345.js
-
-[mitm]
-hostname = 101.34.60.156
-
-*************************************/
-
-/*************************************
-
-项目名称：4k影视
-下载地址：
-更新日期：2024-10-10
-脚本作者：
-电报频道：
-使用声明：⚠️仅供参考，🈲转载与售卖！
-
-**************************************
-
-[rewrite_local]
-^http?:\/\/101\.34\.60\.156:6543\/sk-api\/user\/open_vip url script-response-body https://raw.githubusercontent.com/Hyy800/Quantumult-X/refs/heads/Nana/123.js
+^http:\/\/101\.34\.60\.156:6543\/sk-api\/vod\/skjson\?url url script-response-body https://raw.githubusercontent.com/Hyy800/Quantumult-X/refs/heads/Nana/12345.js
 
 [mitm]
 hostname = 101.34.60.156
@@ -43,19 +24,16 @@ const Hyy666 = typeof $task !== "undefined";
 // 创建自定义会话响应
 hyy = {
   "data": {
-    "user_id": 459,
-    "user_nick_name": "永久会员",
-    "expiretime": null, // 表示没有过期
-    "paytype": 1,
-    "user_points": 9999,
-    "user_phone": "1",
-    "user_extend": 1,
-    "group_id": 1,
-    "user_qq": "1"
+    "isPlayLoading": true,
+    "currentUrlStatus": "loading",
+    "hasNext": false,
+    "code": 200,
+    "currentIndex": 0,
+    "mingwei": 0,
   },
-  "code": 200,
-  "message": "获取会员信息成功"
+  "code": 200
 };
 
 // 返回自定义响应
 $done({status: Hyy666 ? "HTTP/1.1 200 OK" : 200, body: JSON.stringify(hyy)});
+
