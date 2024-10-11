abae2189571d3b4eb9baa9007192e133fb799f02
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-12 03:48:53 +0800

    Update miaobo4Kjiami.js

--- pojie/miaobo4Kjiami.js
+++ pojie/miaobo4Kjiami.js
@@ -1,1 +1,16 @@
+/*************************************
+
+项目名称：淼播4k-强制登陆永久会员账号
+更新日期：2024-10-10
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^http?:\/\/101\.34\.60\.156:6543\/sk-api\/user\/open_vip url script-response-body https://raw.githubusercontent.com/Hyy800/Quantumult-X/refs/heads/Nana/1234.js
+
+[mitm]
+hostname = 101.34.60.156
+
+*************************************/
 var _0x3f3742=_0x78a9;function _0x1611(){var _0x17ce8b=['APP_zjCKZrQo','1032izLdRZ','parse','96xmMBBB','undefined','stringify','894UJKcPC','11xvkRBf','22338MOINHQ','13363Tqnkin','1940590vPRJUU','466248TnpDPs','144134ZkIdDT','2AEGMxt','rebpvb','2887677eJIFmD','18633999183','HTTP/1.1\x20200\x20OK','500cYWsSj','df2887c6f38d624e3b7d3e6b42c45de5'];_0x1611=function(){return _0x17ce8b;};return _0x1611();}(function(_0x5eab5b,_0x34ed91){var _0x4a86f6=_0x78a9,_0x2d29bf=_0x5eab5b();while(!![]){try{var _0x38ff0b=-parseInt(_0x4a86f6(0x1b8))/0x1*(parseInt(_0x4a86f6(0x1b7))/0x2)+parseInt(_0x4a86f6(0x1b3))/0x3*(parseInt(_0x4a86f6(0x1c2))/0x4)+parseInt(_0x4a86f6(0x1bd))/0x5*(parseInt(_0x4a86f6(0x1c5))/0x6)+-parseInt(_0x4a86f6(0x1b4))/0x7*(-parseInt(_0x4a86f6(0x1c0))/0x8)+-parseInt(_0x4a86f6(0x1ba))/0x9+-parseInt(_0x4a86f6(0x1b5))/0xa*(-parseInt(_0x4a86f6(0x1c6))/0xb)+parseInt(_0x4a86f6(0x1b6))/0xc;if(_0x38ff0b===_0x34ed91)break;else _0x2d29bf['push'](_0x2d29bf['shift']());}catch(_0x1ec340){_0x2d29bf['push'](_0x2d29bf['shift']());}}}(_0x1611,0x32baf));var hyy=JSON[_0x3f3742(0x1c1)]($response['body']);function _0x78a9(_0x105608,_0x7140ad){var _0x161199=_0x1611();return _0x78a9=function(_0x78a94a,_0x7e0be5){_0x78a94a=_0x78a94a-0x1b3;var _0x408dad=_0x161199[_0x78a94a];return _0x408dad;},_0x78a9(_0x105608,_0x7140ad);}const Hyy666=typeof $task!==_0x3f3742(0x1c3);hyy={'data':{'tgm':_0x3f3742(0x1b9),'user_points':0x1,'user_pid_2':0x0,'user_id':0x9,'user_phone':_0x3f3742(0x1bb),'user_extend':0x0,'user_reg_time':0x66edb599,'user_name':_0x3f3742(0x1bb),'group_id':0x3,'user_email':'','user_pid':0x0,'user_nick_name':_0x3f3742(0x1bf),'user_qq':'','user_random':_0x3f3742(0x1be),'user_portrait':'','user_end_time':0xf2a52380},'code':0xc8},$done({'status':Hyy666?_0x3f3742(0x1bc):0xc8,'body':JSON[_0x3f3742(0x1c4)](hyy)});
