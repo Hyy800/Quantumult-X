a0e41e588b026cacb1349f7e6b222eb906de2ccc
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-12 02:37:02 +0800

    更新 xiaoguaijizhang2.js

--- pojie/xiaoguaijizhang2.js
+++ pojie/xiaoguaijizhang2.js
@@ -14,6 +14,28 @@ hostname = tcbff.xiaoguaijizhang.cn
 var hyy = JSON.parse($response.body);
 const Hyy666 = typeof $task !== "undefined";
 
-hyy = {"code":0,"message":"successful","data":{"virtual":{},"_id":"66fb1b3012a137a45a8096b0","vip":3,"create_time":1727732527,"vip_create_time":1727732527,"phone":"","wechat_id":"o2GVx6R36oX0K2KRDg5_kmBSgucw","headimgurl":"https://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEIovZPhFHiaetGsibx51bGtYgEd4BYcUj85cjuYShgMDAnmJuxewJrha8j04iccEqKxYyV8xH8gSShLmvrg4H1VuBD6FLwwc0fztrSV2BES63m4Q/132","nick_name":"f09f8d8a","wechat_unionid":"opPgJ6hdI71n_2Uo4UDPRsn_Lx7I","os_type":"ios","channel":"AppStore","ver":"2.2.5","isVip":true,"membership_type":1}};
+if (hyy && hyy.data && hyy.data.isVip) {
+    hyy = {
+        "code": 0,
+        "message": "successful",
+        "data": {
+            "virtual": {},
+            "_id": "66fb1b3012a137a45a8096b0",
+            "vip": 3,
+            "create_time": 1727732527,
+            "vip_create_time": 1727732527,
+            "phone": "",
+            "wechat_id": "o2GVx6R36oX0K2KRDg5_kmBSgucw",
+            "headimgurl": "https://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEIovZPhFHiaetGsibx51bGtYgEd4BYcUj85cjuYShgMDAnmJuxewJrha8j04iccEqKxYyV8xH8gSShLmvrg4H1VuBD6FLwwc0fztrSV2BES63m4Q/132",
+            "nick_name": "f09f8d8a",
+            "wechat_unionid": "opPgJ6hdI71n_2Uo4UDPRsn_Lx7I",
+            "os_type": "ios",
+            "channel": "AppStore",
+            "ver": "2.2.5",
+            "isVip": true,
+            "membership_type": 1
+        }
+    };
+}
 
 $done({status: Hyy666 ? "HTTP/1.1 200 OK" : 200, body: JSON.stringify(hyy)});
