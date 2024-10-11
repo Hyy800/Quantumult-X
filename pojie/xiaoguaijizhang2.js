90a265efbf923716dab6536cce1890e9e8e5b74a
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-12 02:53:59 +0800

    更新 xiaoguaijizhang2.js

--- pojie/xiaoguaijizhang2.js
+++ pojie/xiaoguaijizhang2.js
@@ -11,9 +11,36 @@ hostname = tcbff.xiaoguaijizhang.cn
 
 *************************************/
 
-var hyy = JSON.parse($response.body);
-const Hyy666 = typeof $task !== "undefined";
+let obj = {};
 
-hyy = {"code":0,"message":"successful","data":{"virtual":{},"_id":"    ","vip":3,"create_time":    ,"vip_create_time":    ,"phone":"","wechat_id":"    ","headimgurl":"https://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEIovZPhFHiaetGsibx51bGtYgEd4BYcUj85cjuYShgMDAnmJuxewJrha8j04iccEqKxYyV8xH8gSShLmvrg4H1VuBD6FLwwc0fztrSV2BES63m4Q/132","nick_name":"   ","wechat_unionid":"   ","os_type":"ios","channel":"AppStore","ver":"2.2.5","isVip":true,"membership_type":1}};
+if(typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  obj.headers = $request.headers;
+} else {
+  let body = JSON.parse(typeof $response != "undefined" && $response.body || null);
+  // 替换为提供的 JSON 数据
+  obj.body = JSON.stringify({
+    "code": 0,
+    "message": "successful",
+    "data": {
+      "virtual": {},
+      "_id": "66fb1b3012a137a45a8096b0",
+      "vip": 3,
+      "create_time": 1727732527,
+      "vip_create_time": 1727732527,
+      "phone": "",
+      "wechat_id": "o2GVx6R36oX0K2KRDg5_kmBSgucw",
+      "headimgurl": "https://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEIovZPhFHiaetGsibx51bGtYgEd4BYcUj85cjuYShgMDAnmJuxewJrha8j04iccEqKxYyV8xH8gSShLmvrg4H1VuBD6FLwwc0fztrSV2BES63m4Q/132",
+      "nick_name": "f09f8d8a",
+      "wechat_unionid": "opPgJ6hdI71n_2Uo4UDPRsn_Lx7I",
+      "os_type": "ios",
+      "channel": "AppStore",
+      "ver": "2.2.5",
+      "isVip": true,
+      "membership_type": 1
+    }
+  });
+}
 
-$done({status: Hyy666 ? "HTTP/1.1 200 OK" : 200, body: JSON.stringify(hyy)});
+$done(obj);
