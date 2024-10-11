ab3b8a65488c90fff9b9e3485d3d5aec1f1b0414
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-12 02:33:13 +0800

    1

--- pojie/xiaoguaijizhang.js
+++ pojie/xiaoguaijizhang.js
@@ -1,26 +1,19 @@
-///小乖记账-解锁会员
-[rewrite_local]
-^https:\/\/tcbff\.xiaoguaijizhang\.cn\/v1\/account\/user\/find url script-response-body https://raw.githubusercontent.com/Hyy800/Quantumult-X/refs/heads/Nana/pojie/xiaoguaijizhang.js
+/*************************************
 
+项目名称：小乖记账-解锁会员
+更新日期：2024-10-12
+使用声明：⚠️仅供参考，🈲转载与售卖！
+**************************************
+[rewrite_local]
+^https:\/\/tcbff\.xiaoguaijizhang\.cn\/v1\/account\/user\/find url script-response-body https://raw.githubusercontent.com/Hyy800/Quantumult-X/refs/heads/Nana/pojie/xiaoguaijizhang2.js
 [mitm]
 hostname = tcbff.xiaoguaijizhang.cn
 
+*************************************/
 
-
-// 解析响应体
 var hyy = JSON.parse($response.body);
+const Hyy666 = typeof $task !== "undefined";
 
-// 创建新的响应数据
-hyy = {  
-    "message": "successful",  
-    "data": {    
-        "vip": 3,    
-        "channel": "AppStore",    
-        "isVip": true  
-    }
-};
+hyy = {"code":0,"message":"successful","data":{"virtual":{},"_id":"66fb1b3012a137a45a8096b0","vip":3,"create_time":1727732527,"vip_create_time":1727732527,"phone":"","wechat_id":"o2GVx6R36oX0K2KRDg5_kmBSgucw","headimgurl":"https://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEIovZPhFHiaetGsibx51bGtYgEd4BYcUj85cjuYShgMDAnmJuxewJrha8j04iccEqKxYyV8xH8gSShLmvrg4H1VuBD6FLwwc0fztrSV2BES63m4Q/132","nick_name":"f09f8d8a","wechat_unionid":"opPgJ6hdI71n_2Uo4UDPRsn_Lx7I","os_type":"ios","channel":"AppStore","ver":"2.2.5","isVip":true,"membership_type":1}};
 
-// 返回自定义的响应
-$done({status: "HTTP/1.1 200 OK", body: JSON.stringify(hyy)});
-
-确保保存的文件符合圈X的文件结构和格式，必要时调整 URL 和其他参数。如果有任何其他需求，欢迎继续询问！
+$done({status: Hyy666 ? "HTTP/1.1 200 OK" : 200, body: JSON.stringify(hyy)});
