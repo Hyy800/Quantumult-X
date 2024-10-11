bacb3a53bd8539e86c333eeda3741d530de4903f
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-12 06:19:19 +0800

    Update 1234.js

--- 1234.js
+++ 1234.js
@@ -11,29 +11,10 @@ hostname = api.revenuecat.com
 var hyy = JSON.parse($response.body);
 const Hyy666 = typeof $task !== "undefined";
 
-hyy = {
-  "request_date_ms" : 1728683360971,
-  "request_date" : "2024-10-11T21:49:20Z",
-  "subscriber" : {
-    "non_subscriptions" : {
-
-    },
-    "first_seen" : "2024-10-11T21:48:26Z",
-    "original_application_version" : "491",
-    "other_purchases" : {
-
-    },
-    "management_url" : puls,
-    "subscriptions" : {
-
-    },
-    "entitlements" : {
-
-    },
-    "original_purchase_date" : "2024-10-11T21:47:38Z",
-    "original_app_user_id" : "$RCAnonymousID:542a1cc3dca14386a5d282f6a0c2bad6",
-    "last_seen" : "2024-10-11T21:49:04Z"
+var hyy = {
+  "subscriber": {
+    "management_url": "puls"
   }
 };
 
-$done({status: Hyy666 ? "HTTP/1.1 200 OK" : 200, body: JSON.stringify(hyy)});
+$done({status: "HTTP/1.1 200 OK", body: JSON.stringify(hyy)});
