60e9b5e4a81b55fb4977e7f78f7709c5500fb35e
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-12 04:07:45 +0800

    更新 xiaoguaijizhang.js

--- pojie/xiaoguaijizhang.js
+++ pojie/xiaoguaijizhang.js
@@ -10,23 +10,31 @@
 hostname = tcbff.xiaoguaijizhang.cn
 
 *************************************/
+var hyy = JSON.parse($response.body);
+const Hyy666 = typeof $task !== "undefined";
 
-let obj = {};
-
-if(typeof $response == "undefined") {
-  delete $request.headers["x-revenuecat-etag"];
-  delete $request.headers["X-RevenueCat-ETag"];
-  obj.headers = $request.headers;
-} else {
-  let body = JSON.parse(typeof $response != "undefined" && $response.body || null);
-  obj.body = JSON.stringify({
-    "message": "successful",
-    "data": {
-      "vip": 1,
-      "isVip": true,
-      "membership_type": premium
-    }
-  });
+if (hyy && hyy.data && hyy.data.isVip) {
+    hyy = {
+        "code": 0,
+        "message": "successful",
+        "data": {
+            "virtual": {},
+            "_id": "66fb1b3012a137a45a8096b0",
+            "vip": 3,
+            "create_time": 1727732527,
+            "vip_create_time": 1727732527,
+            "phone": "",
+            "wechat_id": "o2GVx6R36oX0K2KRDg5_kmBSgucw",
+            "headimgurl": "https://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEIovZPhFHiaetGsibx51bGtYgEd4BYcUj85cjuYShgMDAnmJuxewJrha8j04iccEqKxYyV8xH8gSShLmvrg4H1VuBD6FLwwc0fztrSV2BES63m4Q/132",
+            "nick_name": "f09f8d8a",
+            "wechat_unionid": "opPgJ6hdI71n_2Uo4UDPRsn_Lx7I",
+            "os_type": "ios",
+            "channel": "AppStore",
+            "ver": "2.2.5",
+            "isVip": true,
+            "membership_type": 1
+        }
+    };
 }
 
-$done(obj);
+$done({status: Hyy666 ? "HTTP/1.1 200 OK" : 200, body: JSON.stringify(hyy)});
