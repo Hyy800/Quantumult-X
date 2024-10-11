b3d2af6a804cf77b7af13dff7f69772866062008
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-12 04:09:20 +0800

    更新 xiaoguaijizhang.js

--- pojie/xiaoguaijizhang.js
+++ pojie/xiaoguaijizhang.js
@@ -2,6 +2,7 @@
 项目名称：小乖记账-解锁会员
 更新日期：2024-10-12
 使用声明：仅供参考，转载与售卖！
+
 **************************************
 [rewrite_local]
 ^https:\/\/tcbff\.xiaoguaijizhang\.cn\/v1\/account\/user\/find url script-response-body https://raw.githubusercontent.com/Hyy800/Quantumult-X/refs/heads/Nana/pojie/xiaoguaijizhang.js
