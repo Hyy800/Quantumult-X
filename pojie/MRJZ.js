53ea74146c042a40f327aefda1631d1262fce9dd
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-12 16:06:37 +0800

    Update MRJZ.js

--- pojie/MRJZ.js
+++ pojie/MRJZ.js
@@ -15,19 +15,17 @@ const Hyy666 = typeof $task !== "undefined";
 hyy = {
   "msg": "请求成功",
   "data": {
-    "vipInfo": {
-      "vipExpDay": 999999,
-      "vipType": "permanent",
-      "vipRole": "vip",
-      "showExpPop": false
+    "vipInfo": { // VIP信息部分
+      "vipExpDay": 36385, // VIP有效天数，设置为99年
+      "vipType": "VIP", // VIP类型
+      "vipRole": "vip", // 用户的VIP角色
+      "showExpPop": true // 设置为显示VIP相关的经验弹窗
     },
-    "activityInfo": {
-      "activityResidualTimeStemp": 86297000,
-      "showRedBookActivity": true
-    },
-
+    "activityInfo": { // 活动信息部分
+      "showRedBookActivity": false // 设置为不显示红书活动
+    }
   },
-  "code": 200
+  "code": 200 // 响应码
 };
 
 $done({status: Hyy666 ? "HTTP/1.1 200 OK" : 200, body: JSON.stringify(hyy)});
