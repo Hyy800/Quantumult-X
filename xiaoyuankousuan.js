665c60504e55193a1c2547bb419575c53db34dbc
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-13 22:37:59 +0800

    Create xiaoyuankousuan.js

---
+++ xiaoyuankousuan.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+^https?:\/\/xyks\.yuanfudao\.com\/bh5\/leo-web-oral-pk\/exercise.+ url 302 https://cdn.jsdelivr.net/gh/CharlieDijkstra/xiaoyuankousuan_ios@refs/heads/main/exercise_new.js
+
+[mitm]
+hostname = xyks.yuanfudao.com
