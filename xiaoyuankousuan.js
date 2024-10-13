524802fb4771fd58f3b23babdc24743ccb42a981
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-13 22:44:56 +0800

    Update xiaoyuankousuan.js

--- xiaoyuankousuan.js
+++ xiaoyuankousuan.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https?:\/\/xyks\.yuanfudao\.com\/bh5\/leo-web-oral-pk\/exercise.+ url https://cdn.jsdelivr.net/gh/CharlieDijkstra/xiaoyuankousuan_ios@refs/heads/main/exercise_new.js
+^https?:\/\/leo\.fbcontent\.cn\/bh5\/leo-web-oral-pk\/exercise[^\/]+\.js/g, 'https://cdn.rawgit.com/CharlieDijkstra/xiaoyuankousuan_ios/refs/heads/main/exercise_new.js
 
 [mitm]
 hostname = xyks.yuanfudao.com
