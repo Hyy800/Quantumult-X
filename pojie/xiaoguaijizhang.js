c412c6a57adbad9567e6151d46395139618667ca
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-13 16:57:22 +0800

    Update xiaoguaijizhang.js

--- pojie/xiaoguaijizhang.js
+++ pojie/xiaoguaijizhang.js
@@ -4,35 +4,34 @@
 脚本作者：Hyy
 **************************************
 [rewrite_local]
-^https:\/\/tcbff\.xiaoguaijizhang\.cn\/v1\/account\/user\/find url script-response-body https://raw.githubusercontent.com/Hyy800/Quantumult-X/refs/heads/Nana/pojie/xiaoguaijizhang.js
+^https:\/\/tcbff\.xiaoguaijizhang\.cn\/v1\/account\/user\/find url script-response-body https://gitee.com/hyy800/quantumult-x/raw/master/xiaoguaijizhang.js
 
 [mitm]
 hostname = tcbff.xiaoguaijizhang.cn
 
 *************************************/
 
-const isTestMode = $request.headers['X-Test-Mode'] === 'true';
-
 var hyy = JSON.parse($response.body);
-
 const Hyy666 = typeof $task !== "undefined";
 
-hyy = {
-    "code": 0,
-    "message": "successful",
-    "data": {
-        "virtual": {},
-        "vip": 1,
-        "isVip": true,
-        "membership_type": 1
-    }
-};
+if (Hyy666) {
+    hyy = {
+        "code": 0,
+        "message": "successful",
+        "data": {
+            "virtual": {},
+            "_id": "66fb1b3012a137a45a8096b0",
+            "vip": 3,
+            "isVip": true,
+            "membership_type": 1
+        }
+    };
+} else {
+    hyy = {
+        "code": 1,
+        "message": "failed",
+        "data": {}
+    };
+}
 
-$done({
-    status: Hyy666 ? "HTTP/1.1 200 OK" : 200, 
-    body: JSON.stringify(hyy),
-    headers: {
-        "Content-Type": "application/json"
-    },
-    testMode: isTestMode
-});
+$done({ status: Hyy666 ? "HTTP/1.1 200 OK" : 200, body: JSON.stringify(hyy) });
