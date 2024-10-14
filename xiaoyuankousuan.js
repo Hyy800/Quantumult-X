049d792088f6e57ff375f0a1f371e0570c8d7e4b
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2024-10-14 23:41:32 +0800

    删

--- xiaoyuankousuan.js
+++
@@ -1,5 +0,0 @@
-[rewrite_local]
-^https?:\/\/leo\.fbcontent\.cn\/bh5\/leo-web-oral-pk\/exercise[^\/]+\.js/g, 'https://cdn.rawgit.com/CharlieDijkstra/xiaoyuankousuan_ios/refs/heads/main/exercise_new.js
-
-[mitm]
-hostname = xyks.yuanfudao.com
